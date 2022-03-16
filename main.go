8e36f8115d2ab3c7990fb3a15a75fa62d655e310
Davi-Arauj <moreiradaavi@gmail.com>
2022-03-16 17:29:35 -0300

    Configurando Rotas

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	
 	"net/http"
+	"strings"
 
 	"encurtUrl/db"
 	"encurtUrl/url"
@@ -10,10 +12,36 @@ import (
 func main() {
 
 	db.Connection()
-	defer db.DB.Close()
-	http.HandleFunc("/", url.CreateEndPoint)
-	http.HandleFunc("/url/", url.ExpandEndPoint)
-	http.HandleFunc("/redirect/", url.RootEndPoint)
+	
+	configurandoServidor()
 
+	defer db.DB.Close()
+
+}
+
+func configurandoServidor() {
+	
+	configurarRotas()
 	http.ListenAndServe(":8080", nil)
+
+}
+
+func configurarRotas() {
+	
+	http.HandleFunc("/create", direcionamentoRotas)
+	http.HandleFunc("/url/", direcionamentoRotas)
+	http.HandleFunc("/redirect/", direcionamentoRotas)
+}
+
+func direcionamentoRotas(w http.ResponseWriter, r *http.Request) {
+	partes := strings.Split(r.URL.Path, "/")
+
+	if r.Method == "POST" {
+		url.CreateEndPoint(w, r)
+	} else if r.Method == "GET" && partes[1] == "url" {
+		url.ExpandEndPoint(w, r)
+	} else if r.Method == "GET" && partes[1] == "redirect" {
+		url.RootEndPoint(w, r)
+	}
+
 }
