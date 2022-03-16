3ca94604780fcfad8c5176788957878f3e59c5a3
Davi-Arauj <moreiradaavi@gmail.com>
2022-03-16 16:44:05 -0300

    Inicializando o projeto e o Banco

---
+++ db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	
+)
+
+var DB *sql.DB
+var err error
+
+func Connection() {
+
+	// dialect := os.Getenv("DIALECT")
+	// host := os.Getenv("HOST")
+	// dbPort := os.Getenv("DBPORT")
+	// user := os.Getenv("USER")
+	// dbname := os.Getenv("NAME")
+	// dbpassword := os.Getenv("PASSWORD")
+	dialect := "postgres"
+	host := "localhost"
+	dbPort := "5432"
+	user := "postgres"
+	dbname := "urls"
+	dbpassword := "dev123456"
+
+	// Database connection string
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbname, dbpassword, dbPort)
+
+	DB, err = sql.Open(dialect, dbURI)
+
+	if err != nil {
+		panic("Conexão mal sucedida  :(")
+	} else {
+		fmt.Println("Conexão ok")
+	}
+	//defer DB.Close()
+}
